991c3150a549baafc60cf67fd872bab4e6147d73
MatFragg <u202318323@upc.edu.pe>
2025-06-17 23:22:37 -0500

    feat: Add Shared Bounded Context

---
+++ src/main/java/com/hampcoders/electrolink/ElectrolinkPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hampcoders.electrolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrolinkPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrolinkPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hampcoders/electrolink/ElectrolinkPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hampcoders.electrolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrolinkPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
