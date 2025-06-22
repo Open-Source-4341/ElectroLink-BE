019120a95142e83c152bb777b7f5010fb73c7753
MatFragg <u202318323@upc.edu.pe>
2025-06-22 15:04:27 -0500

    feat: Add comment to the Application file

--- src/main/java/com/hampcoders/electrolink/ElectrolinkPlatformApplication.java
+++ src/main/java/com/hampcoders/electrolink/ElectrolinkPlatformApplication.java
@@ -3,6 +3,8 @@ package com.hampcoders.electrolink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// This is the main entry point for the Electrolink Platform application.
 @SpringBootApplication
 public class ElectrolinkPlatformApplication {
 
